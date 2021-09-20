88fd573f51b936a820bc90f2c0be047beb017d21
Ravin <ravinhwg@gmail.com>
2021-09-20 18:14:36 +0530

    Create the rating snippet.

---
+++ packages/frontend/app.js
@@ -0,0 +1,17 @@
+const generateNewRatingSnippet = (rating, ratingText) => {
+  const grayStarCount = Math.abs(5 - rating);
+
+  const GRAY_STAR_TEMPLATE = `<img src="/assets/star_gray.svg" class="star" />`;
+  const GOLD_STAR_TEMPLATE = `<img src="/assets/star_gold.svg" class="star" />`;
+  const starsArray = [];
+  for (let i = 0; i < rating; i++) {
+    starsArray.push(GOLD_STAR_TEMPLATE);
+  }
+  for (let i = 0; i < grayStarCount; i++) {
+    starsArray.push(GRAY_STAR_TEMPLATE);
+  }
+  const stars = starsArray.join("");
+
+  const RATING_SNIPET_TEMPLATE = `<div class="review-container"><div class="star-group">${stars}</div><p class="review-text-rating">${rating}</p><p class="review-text-body">, ${ratingText}</p></div>`;
+  return RATING_SNIPET_TEMPLATE;
+};
