4153ed4e3a31f9e8e44026ce3fc5b95310d35429
Ravin <ravinhwg@gmail.com>
2021-09-20 18:40:31 +0530

    Get average reviews from the server.

--- packages/backend/routers/reviews.js
+++ packages/backend/routers/reviews.js
@@ -3,5 +3,5 @@ const reviewController = require("../controllers/reviewController");
 
 router.post("/reviews", reviewController.postReview);
 router.get("/reviews", reviewController.getReviews);
-
+router.get("/reviews/average", reviewController.getAverageReview);
 module.exports = router;
