22d261afa96f1d1703bb26bd395d3cd6085a51e2
Ravin <ravinhwg@gmail.com>
2021-09-20 23:55:41 +0530

    Remove paging. (Not required)

--- packages/backend/controllers/reviewController.js
+++ packages/backend/controllers/reviewController.js
@@ -2,29 +2,15 @@ const { query } = require("../config/db");
 
 module.exports = {
   getReviews: async (req, res) => {
-    // grab the page number and the number of items per page from query string
-    const page = req.query.page || 1;
-    const perPage = req.query.perPage || 10;
-    // validate the page number and per page number
-    if (page < 1 || perPage < 1) {
-      return res.status(400).json({
-        message: "Invalid page or perPage number",
-      });
-    }
-
     try {
       const reviews = await query(
-        `SELECT * FROM reviews ORDER BY created_at DESC OFFSET $1 LIMIT $2`,
-        [Math.abs(page * perPage - perPage), Math.abs(perPage)]
+        `SELECT * FROM reviews ORDER BY created_at DESC`
       );
       const total = await query("SELECT COUNT(*) FROM reviews");
       res.status(200).json({
         reviews: reviews.rows,
         metaData: {
           total: +total.rows[0].count,
-          page,
-          resultsPerPage: perPage,
-          isNextPage: page * perPage < total,
         },
       });
     } catch (error) {
