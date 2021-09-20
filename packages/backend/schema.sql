7333a92d82248540afa45bd37f2d5e43350ad52a
Ravin <ravinhwg@gmail.com>
2021-09-20 21:09:01 +0530

    Add timestamp to database schema.

--- packages/backend/schema.sql
+++ packages/backend/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS reviews (
     id serial NOT NULL PRIMARY KEY,
     star_count numeric(2, 1) NOT NULL,
-    review_text text NOT NULL
+    review_text text NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
