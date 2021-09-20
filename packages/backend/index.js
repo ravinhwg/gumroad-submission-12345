8f79574bb5ba4d20249890633810f5a9f2014e71
Ravin <ravinhwg@gmail.com>
2021-09-20 18:30:07 +0530

    Add cors support.

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const api = require("./entry");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", api);
 app.get("/", (req, res) => {
