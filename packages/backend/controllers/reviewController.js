0071278574f4dfb91e9cc5afdf609f66b24de4b9
Ravin <ravinhwg@gmail.com>
2021-09-20 16:13:40 +0530

    Finish getReviews.

--- packages/backend/controllers/reviewController.js
+++ packages/backend/controllers/reviewController.js
@@ -1,8 +1,32 @@
+const { query } = require("../config/db");
+
 module.exports = {
   getReviews: async (req, res) => {
+    // grab the page number and the number of items per page from query string
+    const page = req.query.page || 1;
+    const perPage = req.query.perPage || 10;
+    // validate the page number and per page number
+    if (page < 1 || perPage < 1) {
+      return res.status(400).json({
+        message: "Invalid page or perPage number",
+      });
+    }
+
     try {
-      // TODO: add database query to get all reviews
-      console.log("getReviews");
+      const reviews = await query(`SELECT * FROM reviews OFFSET $1 LIMIT $2`, [
+        Math.abs(page * perPage - perPage),
+        Math.abs(perPage),
+      ]);
+      const total = await query("SELECT COUNT(*) FROM reviews");
+      res.send({
+        reviews: reviews.rows,
+        metaData: {
+          total: +total.rows[0].count,
+          page,
+          resultsPerPage: perPage,
+          isNextPage: page * perPage < total,
+        },
+      });
     } catch (error) {
       throw new Error(error);
     }
